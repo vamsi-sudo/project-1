ea12d820f9bcdeed0c4e153c58ce2840133f158d
vamsi-sudo <85382005+vamsi-sudo@users.noreply.github.com>
2021-06-11 15:26:40 +0530

    Create problems.cpp

---
+++ problems.cpp
@@ -0,0 +1,26 @@
+//size of datatypes
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"size of char: "<<sizeof(char)<<"byte"<<endl;
+    cout<<"size of int: "<<sizeof(int)<<"byte"<<endl;
+    cout<<"size of float: "<<sizeof(float)<<"bytes"<<endl;
+    cout<<"size of double: "<<sizeof(double)<<"bytes";
+}
+
+
+//product of 2 numbers
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    cout<<"product: "<<a*b<<endl;
+}
